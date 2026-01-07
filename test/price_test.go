d30df039033d509af28dbac8ebe46972fcf4ecbb
Admin <charliez.2xxx@gmail.com>
2026-01-07 11:26:43 +0700

    add PriceTest close #4

--- test/price_test.go
+++ test/price_test.go
@@ -8,7 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestTitle(t *testing.T) {
+func TestPrice(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	valid_book := entity.Books{
@@ -17,11 +17,11 @@ func TestTitle(t *testing.T) {
 		Code:  "BK299322",
 	}
 
-	t.Run("Case:1 Title", func(t *testing.T) {
+	t.Run("Case:2 Price", func(t *testing.T) {
 		v := valid_book
-		v.Title = "Jd"
+		v.Price = 9999
 		ok, err := govalidator.ValidateStruct(v)
 		g.Expect(ok).To(BeFalse())
-		g.Expect(err.Error()).To(Equal("Name of the book must contain 3-100 characters"))
+		g.Expect(err.Error()).To(Equal("Price must between 50 and 5000"))
 	})
 }
