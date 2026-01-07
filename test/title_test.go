9b0aa2515439b836e22137e632db20a01269ce5e
Admin <charliez.2xxx@gmail.com>
2026-01-07 11:36:20 +0700

    title_test close #2

--- test/title_test.go
+++ test/title_test.go
@@ -8,7 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func Title_Test(t *testing.T) {
+func Test_title(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	valid_book := entity.Books{
@@ -19,7 +19,7 @@ func Title_Test(t *testing.T) {
 
 	t.Run("Case:1 Title", func(t *testing.T) {
 		v := valid_book
-		v.Title = "Jpddd"
+		v.Title = "Jd"
 		ok, err := govalidator.ValidateStruct(v)
 		g.Expect(ok).To(BeFalse())
 		g.Expect(err.Error()).To(Equal("Name of the book must contain 3-100 characters"))
